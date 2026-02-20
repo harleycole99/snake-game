5c99ff8e009fbff8c390a4de604e1ef6d3302ada
Harley Cole <harleycole99@gmail.com>
2026-02-20 11:08:31 +0000

    feat: added loop to make snake move

--- snake-game.py
+++ snake-game.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -6,6 +7,7 @@ screen.bgcolor("green yellow")
 screen.title("Snake Game")
 screen.tracer(0)
 
+screen.update()
 
 segments = []
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
@@ -20,10 +22,21 @@ def add_segments(position):
     segments.append(new_segment)
 
 
+screen.update()
+
 for position in starting_positions:
     add_segments(position)
 
 screen.update()
 
-
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+#
 screen.exitonclick()
